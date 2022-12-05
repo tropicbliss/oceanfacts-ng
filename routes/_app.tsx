7ededab8a6bfd832bbc82fe56507b3d6aa379df6
tropicbliss <tropicbliss@protonmail.com>
2022-12-05 19:46:34 +0800

    Add meta tags and update favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,12 @@ export default function App({ Component }: AppProps) {
     <>
       <Head>
         <title>Ocean Facts</title>
+        <meta name="keywords" content="ocean, fish, facts, api" />
+        <meta name="description" content="Get the latest ocean facts" />
+        <meta property="og:title" content="Ocean Facts" key="title" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Get the latest ocean facts" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div class="px-8">
         <Component />
