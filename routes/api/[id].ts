67765299f354ec376f74e52ea8706eb92bd32134
tropicbliss <48951297+tropicbliss@users.noreply.github.com>
2023-02-04 15:38:16 +0800

    E

--- routes/api/[id].ts
+++ routes/api/[id].ts
@@ -5,6 +5,7 @@ const notFound = new Response("Fact not found", { status: 404 });
 
 export const handler: Handlers = {
   GET: (_req, ctx) => {
+    // Check if id is between 0 and fact length - 1.
     const id = parseInt(ctx.params.id);
     if (isNaN(id)) {
       return notFound;
